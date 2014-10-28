135f53705867192c1cbd17eb492a1817efa65e6f
Blake Dayman <daymanblake@gmail.com>
2014-10-28 11:41:54 -0700

    blog files

--- controller/create-db.php
+++ controller/create-db.php
@@ -2,7 +2,7 @@
 	require_once(__DIR__ . "/../model/database.php"); // brings info from database.php to this file and concatentates the directory from here
 	$connection = new mysqli($host, $username, $password); //used to access the database on the sqli server
 	if($connection->connect_error){						// checks to see if the connection has an error
-		die("Error: " . $connection->connect_error);
+		die("Error: " . $connection->connect_error);	// if there is an error the program will die
 	}
 	$exists = $connection->select_db($database); 		//selects the database and checks if it exists
 	if (!$exists) {
