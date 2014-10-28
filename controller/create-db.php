33a50bbca83f61e1da56533377e2b70ca7240529
Blake Dayman <daymanblake@gmail.com>
2014-10-28 12:17:14 -0700

    blog files

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,23 +1,18 @@
-<!DOCTYPE html>
-<html>
-	<?php
-		require_once(__DIR__ . "/../model/database.php"); // brings info from database.php to this file and concatentates the directory from here
-		$connection = new mysqli($host, $username, $password); //used to access the database on the sqli server
-		if($connection->connect_error){						// checks to see if the connection has an error
-			die("Error: " . $connection->connect_error);	// if there is an error the program will die
+<?php
+	require_once(__DIR__ . "/../model/database.php"); // brings info from database.php to this file and concatentates the directory from here
+	$connection = new mysqli($host, $username, $password); //used to access the database on the sqli server
+	if($connection->connect_error){						// checks to see if the connection has an error
+		die("Error: " . $connection->connect_error);	// if there is an error the program will die
+	}
+	$exists = $connection->select_db($database); 		//selects the database and checks if it exists
+	if (!$exists) {										
+		$query = $connection->query("CREATE DATABASE $database"); //creates a database by query called blog_db NOTE: action words are in uppercase
+		if ($query) {											//outputs a statement that the query was true
+			echo "Successfully created database: " . $database;
 		}
-		$exists = $connection->select_db($database); 		//selects the database and checks if it exists
-		if (!$exists) {										
-			$query = $connection->query("CREATE DATABASE $database"); //creates a database by query called blog_db NOTE: action words are in uppercase
-
-			if ($query) {											//outputs a statement that the query was true
-				echo "Successfully created database: " . $database;
-			}
-		}
-		else {												// Outputs that the database already exists
-			echo "database already exists.";
-		}
-
-		$connection->close();							//closes connection 
-	?>
-</html>
+	}
+	else {												// Outputs that the database already exists
+		echo "database already exists.";
+	}
+	$connection->close();							//closes connection 
+?>
--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <html>
 	<head>
+		<!-- bootstrap -->
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+		<!-- mediaquery -->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>
 			Blakes Blog
 		</title>
