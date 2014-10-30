0580781113b8c91b83f81c3823c702b5913d861d
Blake Dayman <daymanblake@gmail.com>
2014-10-30 11:56:19 -0700

    blog files

--- index.php
+++ index.php
@@ -1,17 +1,4 @@
-<html>
-	<head>
-		<!-- bootstrap -->
-		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-		<!-- mediaquery -->
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<!-- Cascading Style Sheets -->
-		<link rel="stylesheet" type="text/css" href="css/index.css">
-		<title>
-			Blakes Blog
-		</title>
-	</head>
-	<body>
+
 		<?php 
 			require_once(__DIR__ . "/controller/create-db.php"); //executes file from here and concatentates the directory from here
 		 ?>
