3e8be2be577c96fa51bb0855ec117d9a670bf97d
Blake Dayman <daymanblake@gmail.com>
2014-10-30 12:04:30 -0700

    blog files

--- view/header.php
+++ view/header.php
@@ -7,6 +7,8 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<!-- Cascading Style Sheets -->
 		<link rel="stylesheet" type="text/css" href="css/index.css">
+		<!-- Google Fonts -->
+		<link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
 		<title>
 			Blakes Blog
 		</title>
