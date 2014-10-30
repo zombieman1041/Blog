700b742c41da56fe49710b0a1109e01e56244401
Blake Dayman <daymanblake@gmail.com>
2014-10-30 11:51:29 -0700

    blog files

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 		<!-- mediaquery -->
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" type="text/css" href="css/index.css">
 		<title>
 			Blakes Blog
 		</title>
