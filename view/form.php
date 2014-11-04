b4e960b58e38a948805c70a25790ea847cb4d5fe
Blake Dayman <daymanblake@gmail.com>
2014-11-04 12:14:51 -0800

    blog files

---
+++ view/form.php
@@ -0,0 +1,15 @@
+	<h1>Create Blog Post</h1>
+
+	<form>
+		<div> <!-- title -->
+			<label for="title">Title: </label>
+			<input type="text" name="title" />
+		</div>
+		<div><!-- write text here! -->
+			<label for="post">Post: </label>
+			<textarea name="post"></textarea>
+		</div>
+		<div>
+			<button>Submit</button>
+		</div>
+	</form>
--- view/navigation.php
+++ view/navigation.php
@@ -1,5 +1,9 @@
+<?php
+	require_once(__DIR__ . "/../model/config.php");//executes file from here and concatentates the directory from here
+?>
 <nav>
 	<ul>
-		<li><a href="/blog/post.php">Blog Post Form</a></li><!-- looks at the web server root which is localhost -->
+		<li><a href="<?php echo $path . "post.php"?>">Blog Post Form</a></li><!-- looks at the web server root which is localhost -->
 	</ul>
 </nav>
+
