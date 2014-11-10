1f45216b6e38d8c85c09973fec8bf3a8beecb7e7
Blake Dayman <daymanblake@gmail.com>
2014-11-10 13:07:06 -0800

    blog files

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,6 +1,18 @@
 <?php
-	$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
-	$post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
+	require_once(__DIR__ . "/../model/database.php"); // brings info from database.php to this file and concatentates the directory from here
+	
+	$connection = new mysqli($host, $username, $password, $database); //used to access the database on the sqli server
+	
+	$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING); //filters the input of the title and sanitizes from title
+	$post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);	////filters the input of the post and sanitizes from post
 
-	echo "<p>Title: $title</p>";
-	echo "<p>Post: $post</p>";
+	$query = $connection->query("INSERT INTO posts SET title = '$title', post = '$post'")
+	if ($query) {
+		# code...
+		echo "<p>Successfully inserted post: $title</p>";
+	}
+	else{
+		echo "<p>$connection->error</p>";
+	}
+	
+	$connection->close();
