d96e2e6e545b09d9a4c4d5c3eb69d6c8b3a1bb17
Blake Dayman <daymanblake@gmail.com>
2014-11-13 12:10:52 -0800

    Completed video 26 & 27: Created a database class and a constructer for our variables

--- controller/create-post.php
+++ controller/create-post.php
@@ -6,7 +6,7 @@
 	$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING); //filters the input of the title and sanitizes from title
 	$post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);	////filters the input of the post and sanitizes from post
 
-	$query = $connection->query("INSERT INTO posts SET title = '$title', post = '$post'"); //creates a table by query
+	$query = $connection->query("INSERT INTO posts SET title = '$title', post = '$post'"); //creates a table by query with a title using the var title and a post with a var post
 	if ($query) {	// checks to see if this statement exists
 		# code...
 		echo "<p>Successfully inserted post: $title</p>"; // lets you know that it succesfully created your post
--- model/database.php
+++ model/database.php
@@ -1,3 +1,16 @@
-<?php // variables below enables our database to connect to the server
+<?php
+class Database {
+	private $connection; //following variables are set to private so they wont be modified by other files
+	private $host;
+	private $username;
+	private $password;
+	private $database;
 
-?>
+	public function __construct($host, $username, $password, $database) {
+		$this->host = $host;
+		$this->username = $username;
+		$this->password = $password;
+		$this->database = $database;
+	}
+}
+
