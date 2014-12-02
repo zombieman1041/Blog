aa526c8af4d4f36094403c9dc61207e8c2b6985f
Blake Dayman <daymanblake@gmail.com>
2014-12-02 12:22:27 -0800

    Completed video 37 and 38: created a registration form and a database table: users. Commented on both vids

--- controller/create-db.php
+++ controller/create-db.php
@@ -15,4 +15,20 @@
 	else{
 		echo "<p>" . $_SESSION["connection"]->error . "</p>";			//lets you know that table:posts already exists
 	}
+
+	$query = $_SESSION["connection"]->query("CREATE TABLE users ("  //creates a table by query
+
+	. "id int(11) NOT NULL AUTO_INCREMENT,"				// creates unique ids for the server and cannot be empty and handles the ids for us 
+	. "username varchar(30) NOT NULL,"					// creates a collumn for a username with a max length of 30 characters and it cant be empty
+	. "email varchar(50) NOT NULL,"						// creates a collumn for a email with a max length of 50 characters and it cant be empty
+	. "password char(128) NOT NULL,"					// creates a collumn for a password with a max length of 128 characters and it cant be empty
+	. "salt char(128) NOT NULL,"						//creates a security against hackers that cant be empty
+	. "PRIMARY KEY(id))");								// allows the tables to be connected and is shown that it is connected by an id
+	if ($query) {									// checks to see if this statement exists
+	# code...
+		echo "<p>Successfully created table: users</p>";   // lets you know that it succesfully created table:posts
+	}
+	else{
+		echo "<p>" . $_SESSION["connection"]->error . "</p>";			//lets you know that table:posts already exists
+	}	
 ?>
---
+++ view/register-form.php
@@ -0,0 +1,17 @@
+<form>
+	<div>
+		<label for="email">Email: </label>  	
+		<input type="text" name="email" />
+	</div>
+	<div>
+		<label for="username">Username: </label>
+		<input type="text" name="username" />		
+	</div>
+	<div>
+		<label for="password">Password: </label>
+		<input type="password" name="password" />		
+	</div>
+	<div>
+		<button type="submit" class="btn btn-default">Submit</button> <!-- Button that sends data given on code above to a file -->
+	</div>	
+</form>
